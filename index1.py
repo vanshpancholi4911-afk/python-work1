1030cc0434144548f875ada4689bfd76f90a604e
vanshpancholi4911-afk <vanshpancholi4911@gmail.com>
2026-03-25 10:47:58 +0530

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,6 @@
+num = int(input("Enter number:"))
+if num % 5 == 0:
+   print("Divisible by 5")
+else:
+   print("not divisible by 5")
+   
---
+++ index2.py
@@ -0,0 +1,5 @@
+p = float(input("Enter principal: "))
+r = float(input("Enter rate: "))
+t = float(input("Enter time: "))
+si = (p * r * t) / 100
+print("Simple Interest =", si)
